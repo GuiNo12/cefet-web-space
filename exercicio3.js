170e201e1186bcec5ad3dce8d3cdac210396f093
GuiNo12 <guilherme.nogueiracoelho@live.com>
2022-05-05 14:52:15 -0300

    Resolução exercício 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,14 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+//Expandir e recolher parágrafos com clique no botão expandir-retrair
+let btnExpandir = document.querySelectorAll('.botao-expandir-retrair');
+for(let pEl of btnExpandir){
+    pEl.addEventListener('click', expandir);
+}
+function expandir(e){
+    let pEl = e.currentTarget;
+    let p = pEl.parentElement;
+    //adicionar classe espandido em cada parágrafo
+    p.classList.toggle('expandido');
+}
